00d545b6c6cdb428a6b10ee05b8f204981b5519d
falsto <falsto@jiseoan.com>
2014-04-26 16:12:38 +0900

    env 로컬패스 수정
    hologram 패스 수정

--- hologram/common/env.php
+++ hologram/common/env.php
@@ -12,7 +12,7 @@ $backup_dir = "d:/backup/hologram";
 
 $localnetwork = "192.168.0";
 */
-/* 테스트서버 */
+/* 테스트서버 
 $db_ip = "127.0.0.1";
 $db_port = "3306";
 $db_user = "root";
@@ -22,6 +22,19 @@ $db_name = "jmandb";
 $db_dir = "/usr/bin/";
 $web_dir = "/var/www/html/hologram";
 $backup_dir = "/home/jman/backup/hologram";
+
+$localnetwork = "192.168.0";
+*/
+/* 개발PC */
+$db_ip = "127.0.0.1";
+$db_port = "3307";
+$db_user = "root";
+$db_pwd = "whdythelqlpwd";
+$db_name = "jmandb";
+
+$db_dir = "c:/Program Files/MariaDB 5.5/bin";
+$web_dir = "D:/Documents/GitHub/Jman-Kiosk-CMS/hologram";
+$backup_dir = "d:/backup/hologram";
 
 $localnetwork = "192.168.0";
 
